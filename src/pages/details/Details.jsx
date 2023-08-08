83906f264fa39f0a346a1fdc4953cc3919fb4313
Aryamann Singh <aryamann.singh21@gmail.com>
2023-08-08 20:22:01 +0530

    fixed small issue in Details file

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,7 +3,7 @@ import "./style.scss";
 import { useParams } from "react-router-dom";
 
 import useFetch from "../../hooks/useFetch";
-import DetailsBanner from "./detailsBanner/detailsBanner";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
 import VideosSection from "./videosSection/VideosSection";
 import Similar from "./carousels/Similar";

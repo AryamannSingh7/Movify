f4ea2d3ddb698d3a1c29e076446e726d491fce6a
Aryamann Singh <aryamann.singh21@gmail.com>
2023-08-08 20:13:36 +0530

    Finished Explore Page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,15 @@ import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
 import TopRated from "./topRated/TopRated";
+import Anime from "./Anime/Anime";
+import Drama from "./Drama/Drama";
+import Horror from "./Horror/Horror";
+import Comedy from "./comedy/Comedy";
+import Romance from "./Romance/Romance";
+import SciFi from "./scifi/SciFi";
+import Thriller from "./Thriller/Thriller";
+import Adventure from "./Adventure/Adventure";
+import Action from "./action/Action";
 const Home = () => {
   return (
     <div className="homePage">
@@ -11,6 +20,15 @@ const Home = () => {
       <Trending />
       <Popular />
       <TopRated />
+      <Anime />
+      <Action />
+      <Adventure />
+      <Comedy />
+      <Drama />
+      <Horror />
+      <Romance />
+      <SciFi />
+      <Thriller />
     </div>
   );
 };
---
+++ src/pages/home/action/Action.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
+
+const Action = () => {
+  const [endpoint, setEndpoint] = useState("movie");
+
+  const { data, loading } = useFetch(`/discover/${endpoint}?with_genres=28`);
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Movies" ? "movie" : "tv");
+  };
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Action</span>
+        <SwitchTabs data={["Movies", "Tv Shows"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
+    </div>
+  );
+};
+
+export default Action;
